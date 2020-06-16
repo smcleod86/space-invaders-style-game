cbb8daa2b7f54e278f2fb50b226315b53ec3d4a1
smcleod86 <mcleod.scott_86@yahoo.com>
2020-06-15 22:00:16 -0700

    resizing

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 var hero = {
-    top: 700,
-    left: 550
+    top: 690,
+    left: 350
 }
 
 var missiles = []
 
 var enemies = [
-    { left: 200, top: 100 },
     { left: 300, top: 100 },
     { left: 400, top: 100 },
     { left: 500, top: 100 },
     { left: 600, top: 100 },
     { left: 700, top: 100 },
     { left: 800, top: 100 },
-    { left: 900, top: 100 },
-    { left: 200, top: 175 },
     { left: 300, top: 175 },
     { left: 400, top: 175 },
     { left: 500, top: 175 },
     { left: 600, top: 175 },
     { left: 700, top: 175 },
-    { left: 800, top: 175 },
-    { left: 900, top: 175 }
+    { left: 800, top: 175 }
+    
 ]
 
 document.onkeydown = function(e) {
